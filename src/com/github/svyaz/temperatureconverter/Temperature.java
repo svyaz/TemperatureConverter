23b58f8569bba91fd7f343f07542dd4a4bcce97a
Max Kuzhelev <maxim.kuzhelev@gmail.com>
2019-04-17 01:18:46 +0700

    Temperature class - написаны конструктор и методы для получения и установки температуры в K, C, F.

--- src/com/github/svyaz/temperatureconverter/Temperature.java
+++ src/com/github/svyaz/temperatureconverter/Temperature.java
@@ -2,24 +2,48 @@ package com.github.svyaz.temperatureconverter;
 
 public class Temperature {
     private static final String EXC_MSG_ILLEGAL_KELVIN_VALUE = "Temperature in Kelvins should be greater than 0";
+    private static final String EXC_MSG_ILLEGAL_CELSIUS_VALUE = "Temperature in Celsius degrees should be greater than -273.15";
+    private static final String EXC_MSG_ILLEGAL_FAHRENHEIT_VALUE = "Temperature in Fahrenheit degrees should be greater than -459.67";
 
     /**
-     * Absolute temperature value (in Kelvins)
+     * Absolute temperature value (Kelvins in fact)
      */
-    private double baseTemp;
+    private double temperature;
 
     public Temperature() {
-        this.baseTemp = 273.15;
+        this.temperature = 273.15 + 36.6;
     }
 
-    public void setBaseTemp(double baseTemp) {
-        if (baseTemp < 0) {
+    public double getTemperatureInKelvins() {
+        return temperature;
+    }
+
+    public double getTemperatureInCelsius() {
+        return temperature - 273.15;
+    }
+
+    public double getTemperatureInFahrenheit() {
+        return 1.8 * (temperature - 273.15) + 32;
+    }
+
+    public void setTemperatureInKelvins(double kelvinsTemp) {
+        if (kelvinsTemp < 0) {
             throw new IllegalArgumentException(EXC_MSG_ILLEGAL_KELVIN_VALUE);
         }
-        this.baseTemp = baseTemp;
+        this.temperature = kelvinsTemp;
     }
 
-    public double getBaseTemp() {
-        return baseTemp;
+    public void setTemperatureInCelsius(double celsiusTemp) {
+        if (celsiusTemp < -273.15) {
+            throw new IllegalArgumentException(EXC_MSG_ILLEGAL_CELSIUS_VALUE);
+        }
+        this.temperature = celsiusTemp + 273.15;
+    }
+
+    public void setTemperatureInFahrenheit(double fahrenheitTemp) {
+        if (fahrenheitTemp < -459.67) {
+            throw new IllegalArgumentException(EXC_MSG_ILLEGAL_FAHRENHEIT_VALUE);
+        }
+        this.temperature = fahrenheitTemp + 273.15;
     }
 }
